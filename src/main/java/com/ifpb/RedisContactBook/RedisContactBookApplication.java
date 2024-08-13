4f8d45902f5f7445596ca34e8f0489c2894bcf14
Iasmim Siqueira <iasmimsas@gmail.com>
2024-08-13 15:57:06 -0300

    Initial commit

---
+++ src/main/java/com/ifpb/RedisContactBook/RedisContactBookApplication.java
@@ -0,0 +1,13 @@
+package com.ifpb.RedisContactBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisContactBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisContactBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ifpb/RedisContactBook/RedisContactBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifpb.RedisContactBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisContactBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
