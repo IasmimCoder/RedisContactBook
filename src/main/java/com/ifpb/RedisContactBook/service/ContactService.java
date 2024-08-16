00e2b96fe3104b4a483638497bf009ce3a7a152e
Iasmim Siqueira <iasmimsas@gmail.com>
2024-08-16 12:21:48 -0300

    Monitoramento de uso de cache concluido

--- src/main/java/com/ifpb/RedisContactBook/service/ContactService.java
+++ src/main/java/com/ifpb/RedisContactBook/service/ContactService.java
@@ -7,7 +7,9 @@ import com.ifpb.RedisContactBook.repository.ContactRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.interceptor.SimpleKey;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -19,6 +21,22 @@ public class ContactService {
     private ContactRepository contactRepository;
 
     private static final Logger logger = LoggerFactory.getLogger(ContactService.class);
+    private final CacheManager cacheManager;
+
+    @Autowired
+    public ContactService(CacheManager cacheManager) {
+        this.cacheManager = cacheManager;
+    }
+
+    public void checkCache() {
+        var cache = cacheManager.getCache("contacts");
+        var value = cache.get(SimpleKey.EMPTY); //O SimpleKey.EMPTY é utilizado para métodos sem parâmetros
+        if (value != null) {
+            logger.info("Usando cache para findAll");
+        } else {
+            logger.info("Nenhum cache encontrado para findAll");
+        }
+    }
 
     public Contact save (Contact contact) {
         if(!validarTelefone(contact.getNumber())){
