a81cfe3f3bc1020fa72272c258d3da03c001614a
Emanuel Júnior <117325229+EmanuelJnr@users.noreply.github.com>
2024-08-18 10:48:41 -0300

    Resolução do problema do Cache...
    Agora o cache funciona para todos os métodos de maneira eficiente, e houve uma realocação dos métodos: validarTelefone e validarDDD, que agora estão em uma classe apropriada "Validations" dentro do pacote "utilities".

--- src/main/java/com/ifpb/RedisContactBook/service/ContactService.java
+++ src/main/java/com/ifpb/RedisContactBook/service/ContactService.java
@@ -4,10 +4,13 @@ import com.ifpb.RedisContactBook.exceptions.NotFoundException;
 import com.ifpb.RedisContactBook.model.Contact;
 import com.ifpb.RedisContactBook.repository.ContactRepository;
 
+import com.ifpb.RedisContactBook.utilities.Validations;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.interceptor.SimpleKey;
 import org.springframework.stereotype.Service;
@@ -38,26 +41,30 @@ public class ContactService {
         }
     }
 
+    @CacheEvict(value = "contacts", allEntries = true)
+    @CachePut(value = "contacts")
     public Contact save (Contact contact) {
-        if(!validarTelefone(contact.getNumber())){
+        if(!Validations.validarTelefone(contact.getNumber())){
             contact.setNumber("Inválido");
         }
-        if(!verificarDDD(contact.getDdd())){
+        if(!Validations.validarDDD(contact.getDdd())){
             contact.setDdd("Inválido");
         }
         return contactRepository.save(contact);
     }
 
+    @CacheEvict(value = "contacts", allEntries = true)
+    @CachePut(value = "contacts")
     public Contact update(String id, Contact contact){
         Optional<Contact> contactOptional = contactRepository.findById(id);
         if (!contactOptional.isPresent())
             throw new NotFoundException("Contato não encontrado!");
         Contact var = contactOptional.get();
 
-        if(!validarTelefone(contact.getNumber())){
+        if(!Validations.validarTelefone(contact.getNumber())){
             contact.setNumber("Inválido");
         }
-        if(!verificarDDD(contact.getDdd())){
+        if(!Validations.validarDDD(contact.getDdd())){
             contact.setDdd("Inválido");
         }
 
@@ -70,6 +77,7 @@ public class ContactService {
         return contactRepository.save(var);
     }
 
+    @Cacheable(value = "contacts", key = "#id")
     public Contact findById(String id) {
         Optional<Contact> contactOptional = contactRepository.findById(id);
         if (!contactOptional.isPresent())
@@ -84,6 +92,8 @@ public class ContactService {
         return contactRepository.findAll();
     }
 
+    @CacheEvict(value = "contacts", allEntries = true)
+    @CachePut(value = "contacts")
     public void deleteById(String id) {
        Optional<Contact> contactOptional = contactRepository.findById(id);
        if (!contactOptional.isPresent())
@@ -91,52 +101,8 @@ public class ContactService {
         contactRepository.deleteById(id);
     }
 
+    @CacheEvict(value = "contacts", allEntries = true)
     public void deleteAll(){
         contactRepository.deleteAll();
     }
-
-    private static boolean validarTelefone(String telefone){
-        //retira todos os caracteres não-numéricos (incluindo espaço,tab, etc)
-       telefone = telefone.replaceAll("\\D","");
-
-        if (telefone.startsWith("0800")) {
-
-            //verifica se tem a qtde de numeros correta
-            if (telefone.length() == 11){
-                return true;
-            }
-            return false;
-        }
-
-        //verifica se tem a qtde de numeros correta
-        if (!(telefone.length() >= 8 && telefone.length() <= 10)) return false;
-
-       //verifica se o numero foi digitado com todos os dígitos iguais
-       java.util.regex.Pattern p = java.util.regex.Pattern.compile(telefone.charAt(0)+"{"+telefone.length()+"}");
-       java.util.regex.Matcher m = p.matcher(telefone);
-       if(m.find()) return false;
-
-       return true;
-    }
-
-    public static boolean verificarDDD(String ddd){
-        
-        String[] DDDs = {
-                "11", "12", "13", "14", "15", "16", "17", "18", "19",
-                "21", "22", "24", "27", "28", "31", "32", "33", "34",
-                "35", "37", "38", "41", "42", "43", "44", "45", "46",
-                "47", "48", "49", "51", "53", "54", "55", "61", "62",
-                "64", "63", "65", "66", "67", "68", "69", "71", "73",
-                "74", "75", "77", "79", "81", "82", "83", "84", "85",
-                "86", "87", "88", "89", "91", "92", "93", "94", "95",
-                "96", "97", "98", "99"};
-
-        for(String d: DDDs){
-            if(ddd.equals(d)){
-                return true;
-            }
-        }
-        return false;
-    }
-
 }
